07e21fbfd45d2bc8df4463e8898c6b12dc23c086
RHG <huskey.rusty@gmail.com>
2017-09-16 23:23:55 -0400

    Update auth.js

--- commands/auth.js
+++ commands/auth.js
@@ -21,8 +21,38 @@ module.exports.run = (bot, message, args) => {
 		message.guild.createRole({
 			name: 'Authorized',
 			color: 'BLUE',
-		}, "I just like making roles!");
-	}.then (role => {
+		}, "I just like making roles!")
+		.then (role => {
+		if (!message.guild.roles.find('name', "Authorized")){
+			console.log("Unabled to create role!")
+			return;
+		};
+		if (message.guild.roles.find('name', 'Authorized').comparePositionTo(message.guild.me.highestRole) > 0) {
+			let em = new Discord.RichEmbed()
+			.setTitle(`${message.author.username}#${message.author.discriminator}`)
+			.setDescription("Couldn't be authorized")
+			.addField("Reason:", "The 'Authorized' role is higher than my role!")
+			.setThumbnail(message.author.displayAvatarURL)
+			.setColor(`#FF0000`)
+			.setTimestamp()
+			.setFooter('ERROR');
+
+			message.channel.send({embed: em});
+			return
+			}
+		message.member.addRole(message.guild.roles.find('name', 'Authorized'), "User has been authorized with AuthBot!")
+		let em = new Discord.RichEmbed()
+			.setTitle(`${message.author.username}#${message.author.discriminator}`)
+			.setDescription("You are now Authorized")
+			.setThumbnail(message.author.displayAvatarURL)
+			.setColor(`#00FF00`)
+			.setFooter(`Now authorized on ${message.guild.name}`)
+			.setTimestamp();
+	
+			message.channel.send({embed: em});
+			return
+			};
+	}
 	if (!message.guild.roles.find('name', "Authorized")){
 		console.log("Unabled to create role!")
 		return;
@@ -42,17 +72,14 @@ module.exports.run = (bot, message, args) => {
 		}
 	message.member.addRole(message.guild.roles.find('name', 'Authorized'), "User has been authorized with AuthBot!")
 	let em = new Discord.RichEmbed()
-	.setTitle(`${message.author.username}#${message.author.discriminator}`)
-    .setDescription("You are now Authorized")
-	.setThumbnail(message.author.displayAvatarURL)
-    .setColor(`#00FF00`)
-	.setFooter(`Now authorized on ${message.guild.name}`)
-	.setTimestamp();
+		.setTitle(`${message.author.username}#${message.author.discriminator}`)
+   	 	.setDescription("You are now Authorized")
+		.setThumbnail(message.author.displayAvatarURL)
+    		.setColor(`#00FF00`)
+		.setFooter(`Now authorized on ${message.guild.name}`)
+		.setTimestamp();
 	
 	message.channel.send({embed: em});
-	};
-	
-	
 }
 
 module.exports.help = {
