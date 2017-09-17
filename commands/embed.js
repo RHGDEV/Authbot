602510cf16f074462edc1aba040d84d8afefb1de
RHG <huskey.rusty@gmail.com>
2017-09-16 23:51:45 -0400

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
 	let em = new Discord.RichEmbed()
    	 .setDescription(args.join(" "))
-		.url("https://github.com/CrawlingArc33/Authbot")
-  	  .setColor(`#9B59B6`);
+	 .setURL("https://github.com/CrawlingArc33/Authbot")
+  	 .setColor(`#9B59B6`);
 	
     message.channel.send({embed: em});
 }
