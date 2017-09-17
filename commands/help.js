b2d1c227299b023570ac047f0a2fa675fa46a685
RHG <huskey.rusty@gmail.com>
2017-09-16 21:48:48 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,6 @@ var prefix = process.env.BOT_PREFIX
 
 module.exports.run = (bot, message, args) => {
 
-	//let msg = await message.channel.send(`${message.author} My commands have been private messaged to you! `, {tts: true})
-
 	let em = new Discord.RichEmbed()
 	 .setColor(`#9B59B6`)
 	 .addField(`${prefix}auth`, `You have agreed to the terms and your account will be authorized.`)
@@ -12,12 +10,8 @@ module.exports.run = (bot, message, args) => {
 	 .addField(`${prefix}embed`, `Create some text that's embed!`)
 	 .addField(`${prefix}invite`, `Get an invite to invite the bot to your server!`)
 	 .addField(`${prefix}repeat`, `let's the bot repeat what you say!`)
-	 //.addField("More  commands coming soon", `.`,)
-	 .setFooter(`${bot.user.username}'s help system`);
 
-	// message.author.dmChannel.send({embed: em});
 	message.channel.send({embed: em});
-	// msg.delete()
 }
 
 module.exports.help = {
