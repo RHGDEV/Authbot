66c5bbe8915cb7ab96d6f2c0e813b3db8736a5fe
RHG <huskey.rusty@gmail.com>
2017-09-16 23:47:24 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,10 +6,12 @@ module.exports.run = (bot, message, args) => {
 
 	let em = new Discord.RichEmbed()
 	 .setColor(`#9B59B6`)
+	 .setTitle("Authbot help")
 	 .addField(`${prefix}auth`, `You have agreed to the terms and your account will be authorized.`)
 	 .addField(`${prefix}info`, `Get your infomation about your account.`)
 	 .addField(`${prefix}embed`, `Create some text that's embed!`)
 	 .addField(`${prefix}invite`, `Get an invite to invite the bot to your server!`)
+	 .url("https://github.com/CrawlingArc33/Authbot")
 	 .addField(`${prefix}repeat`, `let's the bot repeat what you say!`);
 
 	message.channel.send({embed: em});
