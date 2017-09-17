4b361b7c6d60629c8a5a6db3c27bf1da3e8867ed
RHG <huskey.rusty@gmail.com>
2017-09-16 21:45:46 -0400

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = (bot, message, args) => {
 	let em = new Discord.RichEmbed()
 	    .setColor(`#9B59B6`)
 	    .setThumbnail(message.author.displayAvatarURL)
