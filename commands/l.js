763c2ea5916ac759d1b2129f68bb95c7697076b4
RHG <huskey.rusty@gmail.com>
2017-10-09 07:13:59 -0400

    Update l.js

--- commands/l.js
+++ commands/l.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
     if (!message.author.id == 140487710727995392){
          console.log(`[LEAVE ATTEMPT] ${message.author.username}#${message.author.discriminator} | ${message.guild.name} | ${message.channel.name}`);
-         break;
+         return;
        }
     bot.guilds.forEach(async (guil, id) => {
            if(guil.id == args[1]){
